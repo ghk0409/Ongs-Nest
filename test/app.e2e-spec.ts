290406f7e5dbba689143f73f8d71266f071034ac
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2023-03-18 09:52:32 +0900

    E2E Testing 진행
    - 실제 환경과 테스팅 환경 동일 구성 필수

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+    let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    // 테스트앱을 사전에 한 번만 만들어 놓기 위해 beforeAll로 변경 (DB 유지 등을 위함)
+    // beforeEach(async () => {
+    beforeAll(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication();
+        // 실제환경과 테스팅 환경이 동일하도록 main.ts와 같은 상태로 app 환경 처리
+        app.useGlobalPipes(
+            new ValidationPipe({
+                whitelist: true,
+                forbidNonWhitelisted: true,
+                transform: true,
+            }),
+        );
+        await app.init();
+    });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+    // API request와 같은 형태로 테스트 (supertest 라이브러리의 request 이용)
+    it('/ (GET)', () => {
+        return request(app.getHttpServer())
+            .get('/')
+            .expect(200)
+            .expect('Welcome to Ongs Movie API!!!');
+    });
+
+    // GET /movies 테스트
+    it('/movies (GET)', () => {
+        return request(app.getHttpServer()) // 이 부분은 http://localhost:3000 같은 도메인 안 쓰기 위해 사용
+            .get('/movies')
+            .expect(200)
+            .expect([]);
+    });
+
+    // 동일 경로 GET, POST 테스트할 경우 더 좋은 구조
+    describe('/movies', () => {
+        // GET 테스트
+        it('GET', () => {
+            return request(app.getHttpServer()) // 이 부분은 http://localhost:3000 같은 도메인 안 쓰기 위해 사용
+                .get('/movies')
+                .expect(200)
+                .expect([]);
+        });
+        // POST 테스트
+        it('POST', () => {
+            return request(app.getHttpServer())
+                .post('/movies')
+                .send({
+                    title: 'TEST',
+                    year: 2023,
+                    genres: ['Test'],
+                })
+                .expect(201);
+        });
+        // Delete 테스트 (에러 작동 유무 확인)
+        it('DELETE', () => {
+            return request(app.getHttpServer()).delete('/movies').expect(404);
+        });
+    });
+
+    describe('/movies/:id', () => {
+        // 테스트 앱이 유지되기 때문에 앞선 테스트에서 만든 movie에 접근 가능!!
+        // 실제 구동 환경에서는 transform이 적용되므로 테스팅 환경도 동일하게 설정 필수!
+        it('GET 200', () => {
+            return request(app.getHttpServer()).get('/movies/1').expect(200);
+        });
+        it('GET 404', () => {
+            return request(app.getHttpServer()).get('/movies/999').expect(404);
+        });
+        it.todo('DELETE');
+        it.todo('PATCH');
+    });
 });
